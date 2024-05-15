88422937f7d9d82315e49b4020347fb56c71a619
omobolarinwaquadri <quadribola91@gmail.com>
2024-05-15 14:47:44 +0100

    1.1

--- src/components/Message/Message2.js
+++ src/components/Message/Message2.js
@@ -14,27 +14,27 @@ const Message2 = () => {
   };
   return (
     <>
-      <section className="container mx-auto flex gap-12 mb-8 px-4 py-2">
-        <div className="gap-col-3 bg-gray-300 rounded-xl px-6 py-3">
+      <section className="container mx-auto flex flex-col md:flex-row gap-4 mb-8 px-4 py-2">
+        <div className="bg-gray-300 rounded-xl px-6 py-3 w-full md:w-1/4">
           <Link>
-            <img src={pic1} />
+            <img src={pic1} alt="Create New" />
           </Link>
           <div className="mb-4 mt-16">
             <Link to="/" className="flex gap-3">
-              <img src={pic2} />
-              <p>Jobs</p>
+              <img src={pic2} alt="Jobs" />
+              <p className="truncate">Jobs</p>
             </Link>
           </div>
           <div>
             <Link to="/" className="flex gap-3">
-              <img src={pic5} />
-              <p>Candidates</p>
+              <img src={pic5} alt="Candidates" />
+              <p className="truncate">Candidates</p>
             </Link>
           </div>
         </div>
-        <div className="container mx-auto flex flex-col">
+        <div className="container mx-auto flex flex-col w-full md:w-3/4">
           <h1 className="font-bold mb-4">Jobs</h1>
-          <div className="gap-2 mb-4">
+          <div className="flex justify-between items-center mb-4">
             <button className="bg-blue-500 text-white px-3 py-1 rounded-lg mr-3">
               Open and Paused(0) <span className="bg-gray-200">Closed(0)</span>
             </button>
@@ -50,7 +50,7 @@ const Message2 = () => {
             <input
               type="text"
               placeholder="Filter and search jobs"
-              className="w-96 md:px-4 py-1 bg-white text-black text-auto text-center rounded-full ml-1 border-2 md:border-4 outline-none pl-10" // Added left padding for icon
+              className="w-full md:w-96 px-4 py-1 bg-white text-black text-auto text-center rounded-full ml-1 border-2 md:border-4 outline-none pl-10" // Added left padding for icon
               id="search"
               onChange={handleSearchInputChange} // Call handleSearchInputChange function on input change
             />
@@ -60,8 +60,8 @@ const Message2 = () => {
               <h1>Messages (0)</h1>
             </Link>
             <div>
-              <img src={pic3} />
-              <p>No messages to review !</p>
+              <img src={pic3} alt="Group Message" />
+              <p>No messages to review!</p>
             </div>
           </div>
         </div>
